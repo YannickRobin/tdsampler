c6ae81fdaebf4ae8785931473a3a3fe8d10e27c1
yannick.robin@gmail.com <yannick.robin@gmail.com@8419a7e3-49b0-0153-de10-9b0752aa13c4>
2012-06-11 12:08:38 +0000

    [maven-release-plugin]  copy for tag TDSampler-1.4

---
+++ tags/TDSampler-1.4/src/test/java/com/ebizance/tdsampler/TDSamplerTest.java
@@ -0,0 +1,15 @@
+package com.ebizance.tdsampler;
+
+import org.junit.Test;
+
+import com.ebizance.tdsampler.TDSampler;
+
+/**
+ * 
+ * @author Yannick Robin
+ *
+ */
+
+public class TDSamplerTest {
+	
+}
